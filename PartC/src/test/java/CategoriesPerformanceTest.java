be728a2482cf159446e00c3c342f4da1b0b91375
Kevin Luo <91026604+Luo-Kevin@users.noreply.github.com>
2023-11-26 20:45:49 -0500

    update gitignore

--- PartC/src/test/java/CategoriesPerformanceTest.java
+++ PartC/src/test/java/CategoriesPerformanceTest.java
@@ -1,4 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
 
 public class CategoriesPerformanceTest {
+    @Test
+    public void test() {
+        assertEquals(0, 0);
+    }
 
 }
--- PartC/src/test/java/ProjectPerformanceTest.java
+++ PartC/src/test/java/ProjectPerformanceTest.java
@@ -1,4 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class ProjectPerformanceTest {
+    @Test
+    public void test() {
+        assertEquals(0, 0);
+    }
 
 }
