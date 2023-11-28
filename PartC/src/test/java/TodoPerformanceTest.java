e1b1095c1de73bb97489a10b7e941be33e7f5301
Kevin Luo <91026604+Luo-Kevin@users.noreply.github.com>
2023-11-27 21:56:48 -0500

    First iteration

--- PartC/src/test/java/TodoPerformanceTest.java
+++ PartC/src/test/java/TodoPerformanceTest.java
@@ -1,8 +1,92 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import java.io.IOException;
+import java.util.List;
 
-@TestMethodOrder(OrderAnnotation.class)
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import io.restassured.response.Response;
+
 public class TodoPerformanceTest {
+  public Process RunRestAPI;
+
+  @Before
+  public void setUp() throws Exception {
+
+    try {
+      RunRestAPI = Runtime.getRuntime().exec("java -jar runTodoManagerRestAPI-1.5.5.jar");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    Thread.sleep(3000); // wait for server to start
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    RunRestAPI.destroy();
+    Thread.sleep(1000);
+  }
+
+  @Test
+  public void testTodoPerformance() {
+
+    for (int ITERATIONS : Utils.ITERATIONS) {
+
+      // Add iterations amount of todos
+      for (int i = 0; i <= ITERATIONS; i++) {
+        Utils.createTodo("title", "description", false);
+      }
+
+      System.out.println("------------ " + ITERATIONS + " Todos Added ------------");
+
+      // count the number of todos
+      Response response = Utils.getAllTodos("");
+      List<Object> todos = response.jsonPath().getList("todos");
+      System.out.println("Number of todos: " + todos.size());
+
+      // Create a new Todo
+
+      long startTimeForAdd = System.nanoTime();
+      Response newTodo = Utils.createTodo("title", "description", false);
+      long endTimeForAdd = System.nanoTime();
+
+      System.out.println(
+          "Time taken to add a todo: " + (float) (endTimeForAdd - startTimeForAdd) / Utils.millisecondsInNano
+              + " milliseconds");
+
+      int id = Integer.parseInt(newTodo.getBody().jsonPath().getString("id"));
+
+      // Update the Todo
+
+      long startTimeForUpdate = System.nanoTime();
+
+      Utils.updateTodoWithId(id,
+          "new title", "Updated todo", true);
+
+      assertEquals(200, response.getStatusCode());
+
+      long endTimeForUpdate = System.nanoTime();
+
+      System.out.println("Time taken to update a todo: "
+          + (float) (endTimeForUpdate - startTimeForUpdate) / Utils.millisecondsInNano + " milliseconds");
+
+      // Delete the Todo
+
+      long startTimeForDelete = System.nanoTime();
+
+      Response deletedTodo = Utils.deleteTodoWithId(id);
+      assertEquals(200, response.getStatusCode());
+      long endTimeForDelete = System.nanoTime();
+
+      System.out.println("Time taken to delete a todo: "
+          + (float) (endTimeForDelete - startTimeForDelete) / Utils.millisecondsInNano + " milliseconds");
+
+    }
+  }
 
 }
--- PartC/src/test/java/Utils.java
+++ PartC/src/test/java/Utils.java
@@ -1,3 +1,67 @@
+import org.json.simple.JSONObject;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 public class Utils {
+    public static final String baseUrl = "http://localhost:4567";
+    public static int[] ITERATIONS = { 1, 10, 100, 1000, 5000, 10000 };
+    public static final int millisecondsInNano = 1000000;
+
+    public static Response getAllTodos(String queryString) {
+        RestAssured.baseURI = baseUrl;
+        RequestSpecification request = RestAssured.given();
+        Response response;
+
+        if (queryString.equals("")) {
+            response = request.get("/todos");
+
+        } else {
+            response = request.get("/todos?" + queryString);
+        }
+        return response;
+    }
+
+    public static Response createTodo(String title, String description, boolean doneStatus) {
+        RestAssured.baseURI = baseUrl;
+        RequestSpecification request = RestAssured.given();
+
+        request.header("Content-Type", "application/json");
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("title", title);
+        requestParams.put("description", description);
+        requestParams.put("doneStatus", doneStatus);
+
+        request.body(requestParams.toJSONString());
+
+        Response response = request.post("/todos");
+
+        return response;
+    }
+
+    public static Response updateTodoWithId(int id, String title, String description, boolean doneStatus) {
+        RestAssured.baseURI = baseUrl;
+        RequestSpecification request = RestAssured.given();
+
+        request.header("Content-Type", "application/json");
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("title", title);
+        requestParams.put("description", description);
+        requestParams.put("doneStatus", doneStatus);
+
+        request.body(requestParams.toJSONString());
+
+        Response response = request.post("/todos/" + id);
+
+        return response;
+    }
+
+    public static Response deleteTodoWithId(int id) {
+        RestAssured.baseURI = baseUrl;
+        RequestSpecification request = RestAssured.given();
+        Response response = request.delete("/todos/" + id);
+        return response;
+    }
 
 }
